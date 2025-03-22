a0f68edaf54191f0960058d681af65692a3b6681
SaiMaha <145278862+SaiMaha@users.noreply.github.com>
2025-03-22 11:07:44 +0000

    Updated React App with simple webpage

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div style={{ textAlign: "center", padding: "20px" }}>
+        <h1>Welcome to My React App</h1>
+        <p>This is a simple webpage created in GitHub Codespaces!</p>
+      </div>
+    );
+  }
+  
+  export default App;
+  
